c8f41750fd77c308762c80739d215e2b15b898a7
mwychung <maggiechung.wy@gmail.com>
2024-10-07 00:15:02 +0800

    Added two files

---
+++ functions.py
@@ -0,0 +1,12 @@
+# functions.py
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a + b  # Intentional bug to fix later
+
+def multiply(a, b):
+    return a * b
+
+def convert_fahrenheit_to_celsius(fahrenheit):
+    return multiply(subtract(fahrenheit, 32), 9 / 5)
